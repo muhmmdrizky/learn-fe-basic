b2a76cc6313c28bd8c4fa619172f93bc7a52023a
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-05-20 08:52:58 +0700

    init

---
+++ 01-BOM-dan-DOM/05-Mencari-DOM-(Mendapatkan-DOM)/script.js
@@ -0,0 +1,9 @@
+// mendapatkan konten-konten pada elemen dengan tag <head>
+const head = document.head;
+
+// mendapatkan elemen <body>
+const body = document.body;
+
+// mengakses elemen h1 dengan id judul
+const judul = document.getElementById("judul");
+console.log(judul);
