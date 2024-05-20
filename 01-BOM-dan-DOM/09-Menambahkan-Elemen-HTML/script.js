603d547f0b4334fbc49bbc0013e98df8632a0201
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-05-21 05:30:34 +0700

    init

---
+++ 01-BOM-dan-DOM/09-Menambahkan-Elemen-HTML/script.js
@@ -0,0 +1,10 @@
+const newElement = document.createElement("li");
+newElement.innerText = "Selamat menikmati!";
+
+const daftar = document.getElementById("daftar");
+daftar.appendChild(newElement);
+
+const elementAwal = document.createElement("li");
+elementAwal.innerText = "Nyalakan kompor.";
+const itemAwal = document.getElementById("awal");
+daftar.insertBefore(elementAwal, itemAwal);
