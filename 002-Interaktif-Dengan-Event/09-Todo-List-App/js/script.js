903fb5f5212887ee8a8331bdf55de722c35b175f
Muhammad Rizky <muhmmdrizkymr@gmail.com>
2024-05-28 10:59:42 +0700

    init

--- 002-Interaktif-Dengan-Event/09-Todo-List-App/js/script.js
+++ 002-Interaktif-Dengan-Event/09-Todo-List-App/js/script.js
@@ -39,6 +39,53 @@ const addToDo = () => {
   document.dispatchEvent(new Event(RENDER_EVENT));
 };
 
+// Fungsi untuk mencari todo dengan ID yang sesuai pada array todos
+const findTodo = (todoId) => {
+  for (const todoItem of todos) {
+    if (todoItem.id === todoId) {
+      return todoItem;
+    }
+  }
+  return null;
+};
+
+// Fungsi untuk memindahkan todo dari rak “Yang harus dilakukan” ke “Yang sudah dilakukan”
+const addTaskToCompleted = (todoId) => {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+};
+
+const removeTaskFromCompleted = (todoId) => {
+  const todoTarget = findTodoIndex(todoId);
+
+  if (todoTarget === -1) return;
+
+  todos.splice(todoTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+};
+
+const undoTaskFromCompleted = (todoId) => {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+};
+
+const findTodoIndex = (todoId) => {
+  for (const index in todos) {
+    if (todos[index].id === todoId) {
+      return index;
+    }
+  }
+  return -1;
+};
+
 // Fungsi membuat todo
 const makeToDo = (todoObject) => {
   const textTitle = document.createElement("h2");
@@ -67,6 +114,21 @@ const makeToDo = (todoObject) => {
 
     const trashButton = document.createElement("button");
     trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFromCompleted(todoObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(todoObject.id);
+    });
+
+    container.append(checkButton);
   }
 
   return container;
@@ -77,8 +139,15 @@ document.addEventListener(RENDER_EVENT, function () {
   const uncompletedTODOList = document.getElementById("todos");
   uncompletedTODOList.innerHTML = "";
 
+  const completedTODOList = document.getElementById("completed-todos");
+  completedTODOList.innerHTML = "";
+
   for (const todoItem of todos) {
     const todoElement = makeToDo(todoItem);
-    uncompletedTODOList.append(todoElement);
+    if (!todoItem.isCompleted) {
+      uncompletedTODOList.append(todoElement);
+    } else {
+      completedTODOList.append(todoElement);
+    }
   }
 });
